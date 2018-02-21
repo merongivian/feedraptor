1d79d92adc8c7232ec7cf7729ed8016b8ee57bec
Jose Anasco <joseanasco1@gmail.com>
2018-02-20 21:59:46 -0500

    first

---
+++ lib/exfeed.ex
@@ -0,0 +1,18 @@
+defmodule Exfeed do
+  @moduledoc """
+  Documentation for Exfeed.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exfeed.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exfeed_test.exs
@@ -0,0 +1,8 @@
+defmodule ExfeedTest do
+  use ExUnit.Case
+  doctest Exfeed
+
+  test "greets the world" do
+    assert Exfeed.hello() == :world
+  end
+end
