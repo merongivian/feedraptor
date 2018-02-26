84d67d495f5d17a54749821792c88b4e9df2a596
Jose Anasco <joseanasco1@gmail.com>
2018-02-26 14:22:36 -0500

    refactor pattern match defs a bit

--- lib/parsers/xml_parser.ex
+++ lib/parsers/xml_parser.ex
@@ -1,71 +1,51 @@
 defmodule Exfeed.Parser.XML do
   def element({source, map}, name, as: key) do
-    value = node_value(source, name)
-    {source, Map.merge(map, %{key => value})}
+    {source, Map.merge(map, %{key => node_value(source, name)})}
   end
   def element({source, map}, name) do
-    key   = String.to_atom(name)
-    value = node_value(source, name)
-    {source, Map.merge(map, %{key => value})}
+    {source, Map.merge(map, %{String.to_atom(name) => node_value(source, name)})}
   end
 
   def element(source, name, as: key) do
-    value = node_value(source, name)
-    {source, %{key => value}}
+    {source, %{key => node_value(source, name)}}
   end
   def element(source, name) do
-    key   = String.to_atom(name)
-    value = node_value(source, name)
-    {source, %{key => value}}
+    {source, %{String.to_atom(name)=> node_value(source, name)}}
   end
 
   #--------------------------
 
   def element({source, map}, name, as: key, module: module) do
-    value = source
-            |> node_value(name)
-            |> module.create()
-    {source, Map.merge(map, %{key => value})}
+    {source, Map.merge(map, %{key => node_value(source, name, module)})}
   end
   def element({source, map}, name, module: module) do
-    key   = String.to_atom(name)
-    value = source
-            |> node_value(name)
-            |> module.create()
-    {source, Map.merge(map, %{key => value})}
+    {source, Map.merge(map, %{String.to_atom(name) => node_value(source, name, module)})}
   end
 
   def element(source, name, as: key, module: module) do
-    value = source
-            |> node_value(name)
-            |> module.create()
-    {source, %{key => value}}
+    {source, %{key => node_value(source, name, module)}}
   end
   def element(source, name, module: module) do
-    key   = String.to_atom(name)
-    value = source
-            |> node_value(name)
-            |> module.create()
-    {source, %{key => value}}
+    {source, %{String.to_atom(name)=> node_value(source, name, module)}}
   end
 
   #----------------------
 
   def elements({source, map}, name, as: key, module: module) do
-    value = source
-            |> Floki.find(name)
-            |> Enum.map(&module.create/1)
-    {source, Map.merge(map, %{key => value})}
+    {source, Map.merge(map, %{key => node_values(source, name, module)})}
   end
   def elements(source, name, as: key, module: module) do
-    value = source
-            |> Floki.find(name)
-            |> Enum.map(&module.create/1)
-    {source, %{key => value}}
+    {source, %{key => node_values(source, name, module)}}
   end
 
   def parse({_source, map}) do
     map
+  end
+
+  defp node_values(source, name, module) do
+    source
+    |> Floki.find(name)
+    |> Enum.map(&module.create/1)
   end
 
   defp node_value(source, name) do
@@ -80,6 +60,11 @@ defmodule Exfeed.Parser.XML do
       |> List.first
       |> node_value()
     end
+  end
+  def node_value(source, name, module) do
+    source
+    |> node_value(name)
+    |> module.create()
   end
   defp node_value([{_, _, [value]}]) when is_binary(value), do: value
   defp node_value({_, _, [value]}) when is_binary(value), do: value
