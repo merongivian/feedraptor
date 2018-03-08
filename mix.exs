b2d0cd195850645a03e8245ba39399dea44fc73c
Jose Anasco <joseanasco1@gmail.com>
2018-03-08 10:43:45 -0500

    bump to version 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Feedraptor.MixProject do
   def project do
     [
       app: :feedraptor,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
